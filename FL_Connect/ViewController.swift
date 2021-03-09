51f841a9b0f9b027e7c792803b92c756f9f9e536
Sam Zewdu <samzew3@gmail.com>
2021-03-09 10:31:39 -0700

    Basic structure of screen 2

--- FL_Connect/ViewController.swift
+++ FL_Connect/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var useRoomNowPressed: UIButton!
+    
+    struct roomNames {
+        func room(name) -> return String {
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        useRoomNowPressed.isEnabled = false
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showAlertController(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    // Use room now button greyed out unless a 15/30 button is selected
+   
 
 }
 
