8273e888da50e2cba6ca8a5b09c3a8fa14778938
Sam Zewdu <samzew3@gmail.com>
2021-04-05 10:02:13 -0600

    Started rooms controller

---
+++ FL_Connect/RoomsController.swift
@@ -0,0 +1,18 @@
+//
+//  RoomsController.swift
+//  FL_Connect
+//
+//  Created by Sam Zewdu on 4/5/21.
+//
+
+import Foundation
+import UIKit
+
+class room {
+ 
+    var roomName = ""
+    var isBooked = true
+    
+    return (\(roomname))
+    
+}
